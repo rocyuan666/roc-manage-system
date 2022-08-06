e7d712619e25b7e3ac326b84b5b7a5107252d01c
rocyuan666 <rocyuan666@163.com>
2022-08-06 12:08:52 +0800

    脚本

--- script/gitpush.js
+++ script/gitpush.js
@@ -1,19 +1,19 @@
 const childProcess = require("child_process");
 childProcess.exec("git pull gitee master", (err) => {
   if (err) {
-    console.log("pull失败");
+    console.log("pull失败", err);
     return;
   }
   childProcess.exec("git push gitee master", (err) => {
     if (err) {
-      console.log("gitee push失败");
+      console.log("gitee push失败", err);
     } else {
       console.log("gitee push成功");
     }
   });
   childProcess.exec("git push github master", (err) => {
     if (err) {
-      console.log("github push失败");
+      console.log("github push失败", err);
     } else {
       console.log("github push成功");
     }
