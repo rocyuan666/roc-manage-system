b3998c1344b13b84b7b1c87b71c6eb1b062f7760
rocyuan <rocyuan666@163.com>
2023-11-16 21:36:03 +0800

    title

--- roc-vue3/src/views/register.vue
+++ roc-vue3/src/views/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="register">
     <el-form ref="registerRef" :model="registerForm" :rules="registerRules" class="register-form">
-      <h3 class="title">大鹏管理系统</h3>
+      <h3 class="title">{{ title }}</h3>
       <el-form-item prop="username">
         <el-input
           v-model="registerForm.username"
@@ -89,6 +89,7 @@ import { useRouter } from 'vue-router'
 import { ElMessageBox } from 'element-plus'
 import { getCodeImg, register } from '@/api/login'
 
+const title = import.meta.env.VITE_APP_TITLE
 const router = useRouter()
 const { proxy } = getCurrentInstance()
 
